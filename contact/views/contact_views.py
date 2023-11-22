dc2e56f8bb59e9cd1adda886dcff676db980bead
Ricardo Solano <corretorsolano@gmail.com>
2023-11-22 15:02:23 -0300

    Organizando os partials

---
+++ contact/views/contact_views.py
@@ -0,0 +1,34 @@
+from contact.models import Contact
+from django.shortcuts import get_object_or_404, render
+
+
+def index(request):
+    contacts = Contact.objects.filter(show=True).order_by('-id')[:10]
+
+    context = {
+        'contacts': contacts,
+        'site_title': 'Contatos - '
+    }
+
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )
+
+
+def contact(request, contact_id):
+    # single_contact = Contact.objects.filter(pk=contact_id).first()
+    single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
+
+    context = {
+        'contact': single_contact,
+        'site_title': site_title
+    }
+
+    return render(
+        request,
+        'contact/contact.html',
+        context
+    )
